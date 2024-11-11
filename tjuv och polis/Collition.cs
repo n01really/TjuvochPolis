ea4f80c727e590e077bb8468a52a87295f0785fd
Richard Trygg Svartling <tryggrichard@gmail.com>
2024-11-11 10:54:20 +0100

    la till några kommentarer

--- tjuv och polis/Collition.cs
+++ tjuv och polis/Collition.cs
@@ -44,7 +44,7 @@ namespace Tjuv_och_polis
                     Inventory itemRobbed = citizen.Items[index];
                     citizen.Items.RemoveAt(index);
                     robber.Items.Add(itemRobbed);
-                    Console.Beep();
+                    Console.Beep();//det är till för att vara iriterande, jag har hört att det är irriterande att bli rånad
                     Thread.Sleep(500);
                     Console.SetCursorPosition(citizen.X, citizen.Y);
                     Console.Write("X");
@@ -79,7 +79,7 @@ namespace Tjuv_och_polis
                     cop.Items.AddRange(robber.Items);
                     robber.Items.Clear();
                     robber.Prison = true;
-                    Console.Beep();
+                    Console.Beep();//presic som att bli tagen av polisen i verkligheten så är detta ljud lika iriteande
                     Thread.Sleep(500);
                     news.Enqueue(DateTime.Now.ToString("HH:mm:ss") + " Tjuven " + robber.Name + " blev tagen av polisen " + cop.Name + "! Plats: " + robber.X + ":" + robber.Y + "                        ");
                     robber.X = jailStartX + 5;
